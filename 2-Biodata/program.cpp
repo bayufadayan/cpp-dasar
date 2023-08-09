ea8a52290a5ed10845bb5a043eb6301ec305a04d
bayufadayan <bayufadayan@gmail.com>
2023-08-09 17:19:16 +0700

    Menambahkan Biodata

---
+++ 2-Biodata/program.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main (){
+
+    //hanya menggunakan cout doang
+    cout << "================================================================" << endl;
+    cout << "----------------------BIODATA DIRI LENGKAP----------------------" << endl;
+    cout << "================================================================" << endl;
+
+    cout << "Nama Lengkap\t: " << "Muhamad Bayu Fadayan" << endl;
+    cout << "TTL\t\t: " << "Bogor, 07 September 2003" << endl;
+    cout << "Umur\t\t: " << "18 Tahun" <<endl;
+    cout << "Agama\t\t: " << "Islam" << endl;
+    cout << "Alamat Rumah\t: " << "Jalan raya Cangkrang No. 08, Cikarawang, Dramaga" <<endl;
+    cout << "No. HP\t\t: " << "085716042693\n\n";
+
+    cout << "-------------------------ASAL SEKOLAH--------------------------" << endl;
+    cout << "SD/MI\t\t: " << "SDIT AL-YASMIN" << endl;
+    cout << "SMP/MTS\t\t: " << "SMP PESAT" << endl;
+    cout << "SMA/SMK/MA\t: " << "MAN 1 KOTA BOGOR\n\n";
+
+    cout << "---------------------DATA PENDIDIKAN SAAT INI------------------" <<endl;
+    cout << "PTN/PTS\t\t: " << "Universitas Pakuan" <<endl;
+    cout << "Jenjang\t\t: " << "S1"<< endl;
+    cout << "Fakultas\t: " << "FMIPA" << endl;
+    cout << "Progam Studi\t: " << "Ilmu Komputer" <<endl;
+    cout << "Semester\t: " << "1" <<endl;
+    cout << "Kelas\t\t: " << "D" << endl;
+
+    cout << "================================================================" << endl;
+    cout << "================================================================\n\n";
+
+    cout << "MUHAMAD BAYU FADAYAN_065121100\n\n";
+
+
+    getch ();
+    return 0;
+}
