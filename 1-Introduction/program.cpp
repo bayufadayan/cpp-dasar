2f218b27287bdde4e3d38f5b13a21da808d8cf44
bayufadayan <bayufadayan@gmail.com>
2023-08-09 17:13:40 +0700

    Introduction to Learn CPP

---
+++ 1-Introduction/program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main () {
+    //menampilkan teks keluaran
+    /* membuat program sederhana untuk menampilkan pesan 
+    “SAYA MAHASISWA SEMESTER 1 ILMU KOMPUTER UNIVERSITAS PAKUAN”
+    */
+
+   cout << "SAYA MAHASISWA SEMESTER 1"<<endl;
+   cout << "PRODI ILMU KOMPUTER"<<endl;
+   cout << "UNIVERITAS PAKUAN"<<endl;
+
+   cout << "MUHAMAD BAYU FADAYAN_065121100\n\n";
+   
+   getch ();
+   return 0;
+}
